3e913ef3c9a07055494ae152c97cb7b7ff841fda
Andrew Brudnak <brudnak@icloud.com>
2019-06-11 23:33:11 -0700

    Todo App form set up with TypeScript

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,12 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 
+type FormElem = React.FormEvent<HTMLFormElement>;
+
 const App = (): JSX.Element => {
+  const [value, setValue] = useState<string>('');
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    setValue('');
+  };
   return (
     <Fragment>
       <h1>Todo List</h1>
-      <form>
-        <input type='text' required />
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
         <button type='submit'>Add Todo</button>
       </form>
     </Fragment>
